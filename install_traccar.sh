a9d224f4ba4d460a5905ef8fe937fe836af15686
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-07-06 17:23:31 +0200

    Update install_traccar.sh

--- install_traccar.sh
+++ install_traccar.sh
@@ -20,6 +20,14 @@ ENABLE_SSL="True"
 ADMIN_EMAIL="admin@example.com"
 ##
 
+#----------------------------------------------------
+# Disable password authentication
+#----------------------------------------------------
+sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config 
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo service sshd restart
+
 #--------------------------------------------------
 # Update Server
 #--------------------------------------------------
